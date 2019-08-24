0cde1b31dbc0d204f8f777919aa4090f56ab3803
Kristian F Jones <me@kristianjones.xyz>
2019-08-24 16:51:49 -0500

    feat: Publish ready; Very WIP.

---
+++ README.md
@@ -0,0 +1,2 @@
+# TS-CaddyFile Parser
+Very WIP. Don't use. Doesn't support all directives. Your CaddyFile written will not have unsupported directives.
--- src/Parser/Process/TLS.ts
+++ src/Parser/Process/TLS.ts
@@ -1,7 +1,7 @@
 // src/Parser/Process/TLS.ts
 import { TLSCoreDirective, TLSDirective } from '../../types';
 
-const TLSCoreExtract = /(?!\stls)\s(?<certificate>\S+)\s(?<key>\S+)(?<!{)/
+const TLSCoreExtract = /(?!\stls)\s(?<certificate>\S+)\s(?<key>\S+)(?<!{)/;
 
 export function ProcessTLSDirective(directive: string): TLSDirective {
   const core = (TLSCoreExtract.exec(directive).groups as unknown) as TLSCoreDirective;
